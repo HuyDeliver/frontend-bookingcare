144cd9ebfa092a8eeb88e88b76b4e8f4ca19e880
HuyDeliver <channelbaby15@gmail.com>
2025-04-07 09:19:11 +0700

    thực hành tạo giao diện Home và chức năng đổi ngôn ngữ

--- src/containers/Homepage/HomePage.js
+++ src/containers/Homepage/HomePage.js
@@ -3,44 +3,106 @@ import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 import './HomeHeader.scss'
+import { FormattedMessage } from 'react-intl';
+import khamchuyenkhoa from '../../assets/images/khamchuyenkhoa.png'
+import khamtuxa from '../../assets/images/khamtuxa.png'
+import khamtongquat from '../../assets/images/khamtongquat.png'
+import dichvuxetnghiem from '../../assets/images/dichvuxetnghiem.png'
+import suckhoetinhthan from '../../assets/images/suckhoetinhthan.png'
+import khamnhakhoa from '../../assets/images/khamnhakhoa.png'
 
 class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isChangelang: false,
+        }
+    }
+    handleChangeLang = () => {
+        this.setState({
+            isChangelang: !this.state.isChangelang
+        })
+    }
 
     render() {
+        console.log("check props: ", this.props)
         return (
-            <div className="home-header-container">
-                <div className="home-header-content">
-                    <div className="left-content">
-                        <i className="fas fa-bars"></i>
-                        <div className="header-logo">
+            <>
+                <div className="home-header-container">
+                    <div className="home-header-content">
+                        <div className="left-content">
+                            <i className="fas fa-bars"></i>
+                            <div className="header-logo">
+                            </div>
                         </div>
-                    </div>
-                    <div className="center-content">
-                        <div className='child-content'>
-                            <div className='menu-title'><b>Chuyên khoa</b></div>
-                            <div className='subs-title'>Tìm bác sĩ theo chuyên khoa</div>
+                        <div className="center-content">
+                            <div className='child-content'>
+                                <div className='menu-title'><FormattedMessage id="homeheader.speciality" /></div>
+                                <div className='subs-title'><FormattedMessage id="homeheader.searchdoctor" /></div>
+                            </div>
+                            <div className='child-content'>
+                                <div className='menu-title'><FormattedMessage id="homeheader.health-facility" /></div>
+                                <div className='subs-title'><FormattedMessage id="homeheader.select-room" /></div>
+                            </div>
+                            <div className='child-content'>
+                                <div className='menu-title'><FormattedMessage id="homeheader.Doctor" /></div>
+                                <div className='subs-title'><FormattedMessage id="homeheader.select-doctor" /></div>
+                            </div>
+                            <div className='child-content'>
+                                <div className='menu-title'><FormattedMessage id="homeheader.fee" /></div>
+                                <div className='subs-title'><FormattedMessage id="homeheader.check-health" /></div>
+                            </div>
                         </div>
-                        <div className='child-content'>
-                            <div className='menu-title'><b>Cơ sở y tế</b></div>
-                            <div className='subs-title'>Chọn bệnh viên phòng khám</div>
-                        </div>
-                        <div className='child-content'>
-                            <div className='menu-title'><b>Bác sĩ</b></div>
-                            <div className='subs-title'>Chọn bác sĩ giỏi</div>
-                        </div>
-                        <div className='child-content'>
-                            <div className='menu-title'><b>Gói khám</b></div>
-                            <div className='subs-title'>Khám sức khỏe tổng quát</div>
-                        </div>
-                    </div>
-                    <div className="right-content">
-                        <div className="menu-support">
-                            <i className='fas fa-question-circle'></i>
-                            Hỗ trợ
+                        <div className="right-content">
+                            <div className="menu-lang" onClick={() => this.handleChangeLang()}>{this.state.isChangelang ? 'EN' : 'VN'}</div>
+                            <div className="menu-support">
+                                <i className='fas fa-question-circle'></i>
+                                <FormattedMessage id="homeheader.help" />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div className="home-header-slider">
+                    <div className="slider-title">
+                        <h1><FormattedMessage id="slider.medical-background" /></h1>
+                        <h2><FormattedMessage id="slider.comprehensive-care" /></h2>
+                    </div>
+                    <div className="slider-search">
+                        <i className='fas fa-search'></i>
+                        <input type="text" value="" placeholder="Tìm bác sĩ, chuyên khoa, bệnh viện..." />
+                    </div>
+                    <div className="slider-option">
+                        <div className='option-menu'>
+                            <div className='option-img'>
+                                <img src={khamchuyenkhoa} alt="" />
+                            </div>
+                            <div>
+                                <h4><FormattedMessage id="slider.specialist-exam" /></h4>
+                            </div>
+                        </div>
+                        <div className='option-menu'>
+                            <div className='option-img'><img src={khamtuxa} alt="" /></div>
+                            <div><h4><FormattedMessage id="slider.telemedicine" /></h4></div>
+                        </div>
+                        <div className='option-menu'>
+                            <div className='option-img'><img src={khamtongquat} alt="" /></div>
+                            <div><h4><FormattedMessage id="slider.general-exam" /></h4></div>
+                        </div>
+                        <div className='option-menu'>
+                            <div className='option-img'><img src={dichvuxetnghiem} alt="" /></div>
+                            <div><h4><FormattedMessage id="slider.medical-test" /></h4></div>
+                        </div>
+                        <div className='option-menu'>
+                            <div className='option-img'><img src={suckhoetinhthan} alt="" /></div>
+                            <div><h4><FormattedMessage id="slider.mental-health" /></h4></div>
+                        </div>
+                        <div className='option-menu'>
+                            <div className='option-img'><img src={khamnhakhoa} alt="" /></div>
+                            <div><h4><FormattedMessage id="slider.dental-checkup" /></h4></div>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 
@@ -48,7 +110,8 @@ class HomePage extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language
     };
 };
 
