76f920c817cc5270549f92c71d3215fe7ce337e9
HuyDeliver <channelbaby15@gmail.com>
2025-08-30 08:32:33 +0700

    Revert "lại fix"
    This reverts commit 9c67982f62eff8ecb69923c863c5a33cfdbb617a.

--- config-overrides.js
+++
@@ -1,5 +0,0 @@
-module.exports = function override(config, env) {
-    // Đây là nơi bạn có thể chỉnh webpack, babel nếu muốn
-    // Hiện tại giữ nguyên để build bình thường trên Vercel
-    return config;
-};
